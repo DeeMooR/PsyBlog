c090dafbac7218290e078dd42433deb61e2346fa
Dmitry Matyshev <deemoor4@gmail.com>
2026-08-03 21:16:13 +0400

    fix: updated docker settings

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
 };
 
